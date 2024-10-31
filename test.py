94598d409b158c0918e1e3c6d350ec07c98746a0
pink10000 <kyletrinh1223@gmail.com>
2024-10-31 16:27:52 -0700

    :)
    Co-authored-by: SammyoRoy <SammyoRoy@users.noreply.github.com>
    Co-authored-by: Mahmoud Abouzeid <mabouzeid04@users.noreply.github.com>
    Co-authored-by: Pranav Mehta <DaPhysikist@users.noreply.github.com>
    Co-authored-by: Aniket Gupta <newracket@users.noreply.github.com>
    Co-authored-by: rahuljones <rahuljones@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,20 @@
+# %% [markdown]
+# # This is a Markdown cell
+# Here’s where you can write explanations, add headers, and format text.
+
+# %%
+# This is a code cell in the .py file. You can write your code here.
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("World")
+
+# %%
+import numpy as np
+
+# %%
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+c = np.matmul(a, b)
+print(c)
+# %%
